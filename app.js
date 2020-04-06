d8d9a572d5dad598a58ea403b0edb4d720977682
Cihat Baran <55471255+CihatBaran@users.noreply.github.com>
2020-04-05 21:52:38 -0400

    setting up UIController (creation of reading input)

--- app.js
+++ app.js
@@ -27,8 +27,34 @@ var budgetController = (function(){
 */
 var UIController =(function(){
 	
-	// do something
-	
+	var DOMstrings = {
+		
+				inputType: '.add__type',
+				inputDescription: '.add__description',
+				inputValue: '.add__value',
+				inputBtn: '.add__btn'
+	}
+
+
+	return { //Inside return is visible from outside;
+		getInput: function(){
+			
+			return {
+				type : document.querySelector(DOMstrings.inputType).value, // inc or exp
+				description :document.querySelector(DOMstrings.inputDescription).value,
+				value : document.querySelector(DOMstrings.inputValue).value,
+			}
+
+		},
+		
+		getDOMstrings: function(){
+			
+			return DOMstrings;
+		}
+		
+	};
+
+
 	
 })();
 
@@ -41,19 +67,28 @@ var UIController =(function(){
 
 var controller = (function(budgetCtrl, UICtrl){
 	
+	var DOM = UICtrl.getDOMstrings();
 	
 	var ctrlAddItem = function(){
 		
 				// 1. Get the field input data
+				
+				var input = UICtrl.getInput();  // we fethched the object;
+				
+				console.log(input);
+				
+				
+				
+				
 				// 2. Add the item to the budget controller
 				// 3. Add the item to the user interface
 				// 4. Calculate the budget
 				// 5. Display the button
-				console.log("DRY === Dont Repeat Yourself")
+				
 	};
 	
 	
-	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+	document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 	
 	document.addEventListener('keypress', function(event){
 		
